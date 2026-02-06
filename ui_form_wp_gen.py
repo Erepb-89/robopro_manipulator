eb9be9285b1577ae2b1c88a4f8f7d87de41a41c8
Erepb-89 <erepb-89@yandex.ru>
2026-02-06 18:16:58 +0300

    Added methods on_traj_list_clicked, on_actions_list_clicked, execute_selected_action, update_actions, trajectory_selected according new ui_form.

--- ui_form_wp_gen.py
+++ ui_form_wp_gen.py
@@ -6,7 +6,6 @@ from PyQt5.QtWidgets import QMainWindow
 
 from commands import Command, CmdType, RobotCommands
 from config import POINTS_PATH, TRAJ_PATH
-from available_trajectories import available_trajectories
 from ui_form import Ui_Form
 from utils import atomic_write_json
 
@@ -28,6 +27,7 @@ class MainWindow(QMainWindow):
             Command(CmdType.REFRESH_WAYPOINTS, {}, source="GUI"))
         self.Waypoints = self.RobotController.get_waypoints_snapshot()
         self.Trajectories = self.RobotController.get_trajectories_snapshot()
+        self.Actions = self.RobotController.get_actions_snapshot()
         self.opc_handler = opc_handler
 
         self.ZGTimer = QtCore.QTimer()
@@ -49,7 +49,7 @@ class MainWindow(QMainWindow):
         self.ui.ActivateZG.toggled.connect(self.manipulator_free_drive)
         self.ui.ActivateSJ.clicked.connect(self.start_simple_joystick)
         self.ui.MoveTrajectory.clicked.connect(self.move_by_selected_trajectory)
-        self.ui.MoveTrajectory.clicked.connect(self.move_by_selected_trajectory)
+        self.ui.ExecuteAction.clicked.connect(self.execute_selected_action)
         self.ui.OutputControl.setCheckable(True)
         self.ui.OutputControl.toggled.connect(self.manipulator_gripper_control)
         self.ui.SavePoint.clicked.connect(self.save_current_position)
@@ -57,9 +57,11 @@ class MainWindow(QMainWindow):
 
         self.ui.comboBox.currentTextChanged.connect(self.waypoint_selected)
         self.ui.TrajectoriesComboBox.currentTextChanged.connect(self.trajectory_selected)
-        self.ui.trajListView.clicked.connect(self.on_list_clicked)
+        self.ui.trajListView.clicked.connect(self.on_traj_list_clicked)
+        self.ui.actionsListView.clicked.connect(self.on_actions_list_clicked)
         self.update_combo_box()
         self.update_trajectories()
+        self.update_actions()
         self.ui.PowerOn.clicked.connect(lambda: self.cmd_queue.put(
             Command(CmdType.POWER, {'state': 1}, source="GUI")
         ))
@@ -68,12 +70,11 @@ class MainWindow(QMainWindow):
         ))
         self.ui.MoveToPoint.clicked.connect(self.move_to_selected_point)
         self.ui.StopMove.clicked.connect(self.stop_drive)
-        # self.ui.webView.load(QtCore.QUrl("https://stackoverflow.com/"))
-        self.ui.webView.load(QtCore.QUrl("http://192.168.88.100:8080/"))
+        # self.ui.webView.load(QtCore.QUrl("http://192.168.88.100:8080/"))
 
         self.show()
 
-    def update_power_button_state(self):
+    def update_power_button_state(self) -> None:
         is_running = (self.RobotController.get_controller_state() == 'run')
         if is_running:
             self.ui.PowerOn.setStyleSheet('background: rgb(124,252,0);')
@@ -102,7 +103,7 @@ class MainWindow(QMainWindow):
                                            f"Failed to save trajectories: {e}")
             return False
 
-    def update_combo_box(self):
+    def update_combo_box(self) -> None:
         items_model = QStandardItemModel()
         for point in self.Waypoints.keys():
             item = QStandardItem(point)
@@ -110,7 +111,7 @@ class MainWindow(QMainWindow):
             items_model.appendRow(item)
         self.ui.comboBox.setModel(items_model)
 
-    def update_trajectories(self):
+    def update_trajectories(self) -> None:
         items_model = QStandardItemModel()
         for traj in self.Trajectories.keys():
             item = QStandardItem(traj)
@@ -119,14 +120,26 @@ class MainWindow(QMainWindow):
         self.ui.TrajectoriesComboBox.setModel(items_model)
         self.ui.trajListView.setModel(items_model)
 
-    def on_list_clicked(self) -> None:
+    def update_actions(self) -> None:
+        items_model = QStandardItemModel()
+        for action in self.Actions.keys():
+            item = QStandardItem(action)
+            item.setEditable(False)
+            items_model.appendRow(item)
+        self.ui.actionsListView.setModel(items_model)
+
+    def on_traj_list_clicked(self) -> None:
         selected = self.ui.trajListView.currentIndex().data()
         self.ui.TrajectoryName.setText(selected)
 
-    def trajectory_selected(self, traj_name):
+    def on_actions_list_clicked(self) -> None:
+        selected = self.ui.actionsListView.currentIndex().data()
+        self.ui.ActionName.setText(selected)
+
+    def trajectory_selected(self, traj_name) -> None:
         self.ui.TrajectoryName.setText(traj_name)
 
-    def waypoint_selected(self, point_name):
+    def waypoint_selected(self, point_name) -> None:
         self.ui.PointName.setText(point_name)
         if point_name in self.Waypoints:
             wp = self.Waypoints[point_name]
@@ -137,10 +150,10 @@ class MainWindow(QMainWindow):
     # def trajectory_selected(self, trajectory_name):
     #     print(trajectory_name)
 
-    def manipulator_command(self, cmd: Command):
+    def manipulator_command(self, cmd: Command) -> None:
         self.cmd_queue.put(cmd)
 
-    def manipulator_gripper_control(self, clamp: bool):
+    def manipulator_gripper_control(self, clamp: bool) -> None:
         try:
             self.manipulator_command(
                 Command(CmdType.IO_SET, {'index': 0, 'value': clamp}, source="GUI"))
@@ -149,7 +162,7 @@ class MainWindow(QMainWindow):
                                            f"Не удалось изменить состояние DO0: {e}")
             self.ui.OutputControl.setChecked(False)
 
-    def manipulator_free_drive(self, activate: bool):  # 2025_09_29
+    def manipulator_free_drive(self, activate: bool) -> None:  # 2025_09_29
         try:
             if activate:
                 self.manipulator_command(
@@ -169,7 +182,7 @@ class MainWindow(QMainWindow):
             if self.ZGTimer.isActive():
                 self.ZGTimer.stop()
 
-    def manipulator_free_drive_old(self, activate: bool):
+    def manipulator_free_drive_old(self, activate: bool) -> None:
         try:
             self.manipulator_command(
                 Command(CmdType.FREE_DRIVE, {'state': 1 if activate else 2},
@@ -179,7 +192,7 @@ class MainWindow(QMainWindow):
                                            f"Zero Gravity toggle failed: {e}")
             self.ui.ActivateZG.setChecked(False)
 
-    def add_current_point_to_trajectory(self):
+    def add_current_point_to_trajectory(self) -> None:
         point_name = self.ui.PointName.text().strip()
         if not point_name:
             QtWidgets.QMessageBox.warning(None, "Warning",
@@ -203,13 +216,12 @@ class MainWindow(QMainWindow):
 
             self.Trajectories[trajectory_name] = new_trajectory
             if self.save_trajectories():
-                self.update_trajectories()
                 QtWidgets.QMessageBox.information(
                     None,
                     "Success",
                     f"Point '{point_name}' successfully added to trajectory {trajectory_name}!")
 
-    def save_current_position(self):
+    def save_current_position(self) -> None:
         point_name = self.ui.PointName.text().strip()
         if not point_name:
             QtWidgets.QMessageBox.warning(None, "Warning",
@@ -260,14 +272,11 @@ class MainWindow(QMainWindow):
             QtWidgets.QMessageBox.critical(None, "Error",
                                            f"Failed to save point: {e}")
 
-    def stop_drive(self):
+    def stop_drive(self) -> None:
         self.manipulator_command(
             Command(CmdType.STOP_MOVE, {}, source="GUI"))
 
-    def move_by_trajectory_1(self):
-        self.opc_handler.set_command(1)
-
-    def move_to_selected_point(self):
+    def move_to_selected_point(self) -> None:
         point_name = self.ui.comboBox.currentText()
         if not point_name:
             QtWidgets.QMessageBox.warning(None, "Warning",
@@ -288,7 +297,7 @@ class MainWindow(QMainWindow):
             QtWidgets.QMessageBox.critical(None, "Error",
                                            f"Failed to move to point: {e}")
 
-    def move_by_selected_trajectory(self):
+    def move_by_selected_trajectory(self) -> None:
         trajectory_name = self.ui.TrajectoryName.text()
         if not trajectory_name:
             QtWidgets.QMessageBox.warning(None, "Warning",
@@ -310,7 +319,23 @@ class MainWindow(QMainWindow):
             QtWidgets.QMessageBox.critical(None, "Error",
                                            f"Failed to move by trajectory: {e}")
 
-    def _zg_tick(self):  # 2025_09_29
+    def execute_selected_action(self) -> None:
+        action_name = self.ui.ActionName.text()
+        if not action_name:
+            QtWidgets.QMessageBox.warning(None, "Warning",
+                                          "Please select an action first!")
+            return
+        try:
+            # команда через OPC
+            self.opc_handler.set_action(action_name)
+
+            QtWidgets.QMessageBox.information(None, "Success",
+                                              f"Executing action '{action_name}'")
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(None, "Error",
+                                           f"Failed to execute action: {e}")
+
+    def _zg_tick(self) -> None:  # 2025_09_29
         try:
             if self.ui.ActivateZG.isChecked():
                 self.manipulator_command(
@@ -327,7 +352,7 @@ class MainWindow(QMainWindow):
             self.ui.ActivateZG.setChecked(False)
             self.ZGTimer.stop()
 
-    def start_simple_joystick(self):
+    def start_simple_joystick(self) -> None:
         try:
             self.manipulator_command(
                 Command(CmdType.START_SIMPLE_JOYSTICK,
