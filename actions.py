a94db7f64299d1bdf80273cff05f20a3fbd5597f
Erepb-89 <erepb-89@yandex.ru>
2026-02-06 18:08:20 +0300

    Added actions.py and actions_2.json

---
+++ actions.py
@@ -0,0 +1,29 @@
+import json
+from collections import namedtuple
+
+Action = namedtuple('Action', [
+    'name',
+    'commands'])
+
+Command = namedtuple('Command', [
+    'name',
+    'cmd_type'])
+
+
+def get_actions():
+    with open("actions_2.json", 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+        actions_dict = {}
+
+        for action in json_data["actions"]:
+            actions_dict[action["name"]] = Action(
+                action.get("name"),
+                [Command(command.get("name"),
+                         command.get("cmd_type")) for command in action.get('commands')])
+
+    return actions_dict
+
+
+actions = get_actions()
+print(actions["rVTOL2_To_VTOL2Battery"].commands)
+print(actions["rVTOL2Battery_To_VTOL2"].commands[0].name)
