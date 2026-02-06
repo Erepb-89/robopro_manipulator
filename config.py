7871de917edcbe8d6bae2f145c9dd66a5915e8a0
Erepb-89 <erepb-89@yandex.ru>
2026-02-06 18:07:45 +0300

    Changed comments

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ TRAJ_PATH = BASE_DIR / "trajectories.json"
 
 # Параметры подключения
 ROBOT_IP = "127.0.0.1"
-# OPC_ENDPOINT = "opc.tcp://0.0.0.0:4840"
-OPC_ENDPOINT = "opc.tcp://127.0.0.1:4840"
+# OPC_ENDPOINT = "opc.tcp://0.0.0.0:4840"  # для отладки по месту
+OPC_ENDPOINT = "opc.tcp://127.0.0.1:4840"  # для теста
 
 # IO и прочее
 NUM_DIGITAL_IO = 24
