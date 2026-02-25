c21b672f2646c6839c83104c805423ac55336c20
Erepb-89 <erepb-89@yandex.ru>
2026-02-25 08:06:19 +0300

    comment

--- routes.py
+++ routes.py
@@ -18,5 +18,5 @@ def get_routes():
 
 
 routes = get_routes()
-print(routes["rHomePosition_To_Helicopter1"].trajectories)
-print(routes["rHelicopter2Load_To_HomePosition"].trajectories)
+# print(routes["rHomePosition_To_Helicopter1"].trajectories)
+# print(routes["rHelicopter2Load_To_HomePosition"].trajectories)
